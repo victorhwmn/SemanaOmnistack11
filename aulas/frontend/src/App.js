63f73f1756f84545a1c1de3c1cd04ad9e2249af4
Victor Watanabe <victorhwmn@gmail.com>
2020-03-25 16:53:20 -0300

    Aula3

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          Hello Omnistack
-        </p>
-      </div>
-    );
-  }
+//JSX
+import Routes from './routes';
+
+import './global.css';
+
+
+function App() {
+
+  return (
+    <Routes />
+  );
 }
 
 export default App;
